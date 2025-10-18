0d7a291be5519f1c1748fad6903d2d0f73f1d001
yanellaft <yanellaft@gmail.com>
2025-10-18 16:28:22 -0400

    made code neater

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
+//Create a 2D array
 function make2DArray(cols, rows) {
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
+    //fill arrays with 0s
     for (let j = 0; j < arr[i].length; j++) {
       arr[i][j] = 0;
     }
@@ -9,11 +11,23 @@ function make2DArray(cols, rows) {
   return arr;
 }
 
+//the grid
 let grid;
-let w = 10;
+//how big each square is
+let w = 5;
 let cols, rows;
 
 let hueValue = 200;
+
+//check to see if column is in bounds
+function withinCols(i) {
+  return i >= 0 && i <= cols - 1;
+}
+
+//check to see if row is in bounds
+function withinRows(j) {
+  return j >= 0 && j <= rows - 1;
+}
 
 function setup() {
   createCanvas(600, 700);
@@ -21,31 +35,27 @@ function setup() {
   cols = width / w;
   rows = height / w;
   grid = make2DArray(cols, rows);
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[i][j] = 0;
-    }
-  }
 }
 
 function mouseDragged() {
   let mouseCol = floor(mouseX / w);
   let mouseRow = floor(mouseY / w);
 
-  let matrix = 3;
+  //randomly add an area of sand around mouse click
+  let matrix = 10; //add dragger so user can change this value
   let extent = floor(matrix/2);
   for (let i = -extent; i <= extent; i++) {
     for (let j = -extent; j <= extent; j ++) {
       if (random(1) < 0.75) {
         let col = mouseCol + 1;
         let row = mouseRow + j;
-        if (col >= 0 && col <= cols -1 && row >= 0 && row <= rows - 1){
+        if (withinCols(col) && withinRows(row)) {
           grid[col][row] = hueValue;
         }
       }
     }
   }
+  //change the color of the sand
   hueValue += 1;
   if (hueValue > 360) {
     hueValue = 1;
@@ -55,6 +65,7 @@ function mouseDragged() {
 function draw() {
   background(0);
 
+  //draw the sand
   for (let i = 0; i < cols; i ++) {
     for (let j = 0; j < rows; j++) {
       noStroke();
@@ -67,34 +78,44 @@ function draw() {
     }
   }
 
+  //create an array for the next frame
   let nextGrid = make2DArray(cols, rows);
+  //check every cell
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
+      //what is the state? (0 -no color or 1 -sand color)
       let state = grid[i][j];
+
+      //if there is a piece of sand
       if (state > 0) {
+        //check below
         let below = grid[i][j+1];
+
+        //randomly chooses a direction to fall to
         let dir = 1;
         if (random(1) < 0.5) {
           dir *= -1;
         }
 
-        let belowA, belowB;
-
-        if (i + dir >= 0 && i + dir <= cols -1 ) {
-        belowA = grid[i + dir][j + 1];
+        //check below left and right
+        let belowA = -1;
+        let belowB = -1;
+        if (withinCols(i + dir)) {
+          belowA = grid[i + dir][j + 1];
         } 
-        if (i - dir >= 0 && i - dir <= cols -1 ) {
-        belowB = grid[i - dir][j + 1];
+        if (withinCols(i - dir)) {
+          belowB = grid[i - dir][j + 1];
         }
         
+        //move sand down/right/left
         if (below === 0) {
-          nextGrid[i][j + 1] = grid[i][j];
+          nextGrid[i][j + 1] = state;
         } else if (belowA === 0 ){
-          nextGrid[i + dir][j + 1] = grid[i][j];
+          nextGrid[i + dir][j + 1] = state;
         } else if (belowB === 0) {
-          nextGrid[i - dir][j + 1] = grid[i][j];
-        } else {
-          nextGrid[i][j] = grid[i][j];
+          nextGrid[i - dir][j + 1] = state;
+        } else { //stay put
+          nextGrid[i][j] = state;
         }
       }
     }
